f49b44911d2faa3da95b020d1732f61f9667062d
MCSQNXY <mcsqnxy@qq.com>
2025-12-03 23:34:43 +0800

    Add files via upload

--- SecludedLauncher.out.daemon.sh
+++ SecludedLauncher.out.daemon.sh
@@ -3,14 +3,21 @@
 ulimit -s 524288 && chmod -R 777 "$PWD" && export LD_LIBRARY_PATH=.
 
 while true; do
-    ./SecludedLauncher.out.sh
+    ./SecludedLauncher.out --cli
     
     exit_code=$?
+
+    if [[ "$exit_code" -eq 0 && -d "upgrade" ]]; then
+        cp -rf upgrade/* ./ 
+        rm -rf upgrade
+        continue
+    fi
 
     if [ "$exit_code" -ne 0 ]; then
         echo "code == $exit_code"
         sleep 1
-    else
-        break
+        continue
     fi
+    
+    break
 done
--- SecludedLauncher.out.sh
+++ SecludedLauncher.out.sh
@@ -3,13 +3,13 @@
 ulimit -s 524288 && chmod -R 777 "$PWD" && LD_LIBRARY_PATH=.
 
 while true; do
-    ./SecludedLauncher.out 
+    ./SecludedLauncher.out --cli
     
     exit_code=$?
 
-    if [[ "$exit_code" -eq 0 && -d "updater" ]]; then
-        cp -rf updater/* ./ 
-        rm -rf updater
+    if [[ "$exit_code" -eq 0 && -d "upgrade" ]]; then
+        cp -rf upgrade/* ./ 
+        rm -rf upgrade
         continue
     fi
 
